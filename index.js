27f5ad3c84ed8ebe1dd30dae8f8a426eb2218bda
shreiya chowdhary <shreiya.c@gmail.com>
2016-07-27 20:58:18 -0400

    changed the url

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ console.log('i am alive');
 //         $("#askModal").modal();
 //     });
 
-var url = "http://localhost:3000"; //For Local Dev
-// var url = 'https://dry-caverns-14430.herokuapp.com' //For Heroku
+// var url = "http://localhost:3000"; //For Local Dev
+var url = 'https://dry-caverns-14430.herokuapp.com' //For Heroku
 var originList;
 var destinationList;
 var resultDistance;
